85824c48b949ea9ef54bff9012ecee0247f07a1a
saurabh2160 <saurabhmanohar90@gmail.com>
2022-06-12 16:27:35 +0530

    admin model

---
+++ src/models/admin.model.js
@@ -0,0 +1,108 @@
+const mongoose = require('mongoose')
+
+const adminSchema= new mongoose.Schema({
+    name:String,
+    email:String,
+    emailVerified:Boolean,
+    phone:String,
+    password:{
+        type:String,
+        minlength:8,
+        maxlength:15
+    },
+    role: {
+        product: {
+            create:{
+                type:Boolean,
+                default:false
+            },
+            update: {
+                type:Boolean,
+                default:false
+            },
+            Delete:  {
+                type:Boolean,
+                default:false
+            },
+        },
+        user: {
+            create: {
+                type:Boolean,
+                default:false
+            },
+            update: {
+                type:Boolean,
+                default:false
+            },
+            Delete: {
+                type:Boolean,
+                default:false
+            },
+        },
+        wishList: {
+            create: {
+                type:Boolean,
+                default:false
+            },
+            update: {
+                type:Boolean,
+                default:false
+            },
+            Delete: {
+                type:Boolean,
+                default:false
+            },
+        },
+        comment: {
+            create: {
+                type:Boolean,
+                default:false
+            },
+            update: {
+                type:Boolean,
+                default:false
+            },
+            Delete: {
+                type:Boolean,
+                default:false
+            },
+        },
+        cart: {
+            create: {
+                type:Boolean,
+                default:false
+            },
+            update:  {
+                type:Boolean,
+                default:false
+            },
+            Delete: {
+                type:Boolean,
+                default:false
+            },
+        },
+        order: {
+            create: {
+                type:Boolean,
+                default:false
+            },
+            update: {
+                type:Boolean,
+                default:false
+            },
+            Delete: {
+                type:Boolean,
+                default:false
+            },
+        }
+    },
+    isDeleted:{
+        type:Boolean,
+        default:false
+    },
+    deletedAt:{
+        type:Date
+    }
+},{timestamps:true})
+
+module.exports=mongoose.model('admin',adminSchema)
