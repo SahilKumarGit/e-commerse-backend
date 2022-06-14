c7433121938ae7d93701280750e0ba56ec0ac468
saurabh2160 <saurabhmanohar90@gmail.com>
2022-06-14 23:53:10 +0530

    controller update

---
+++ src/controllers/cart/cart.Controller.js
@@ -0,0 +1,5 @@
+// cart create api
+// product add kerga aur increa kerga by 1 [product id and size need to be match then increase] else addd new
+//decrease is also same as update
+//delete is emptys entire itmes
+//view cart 
