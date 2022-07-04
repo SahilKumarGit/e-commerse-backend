b9ce595d08fbb45e13151584e8d4c6f2196a4c0a
SahilKumarGit <email.sahil@yahoo.com>
2022-07-04 20:20:12 +0530

    command from node!

--- src/models/order.model.js
+++ src/models/order.model.js
@@ -106,4 +106,4 @@ const cartSchema = new mongoose.Schema({
     }]
 }, { timestamps: true })
 
-module.exports = mongoose.model('Cart', cartSchema)
+module.exports = mongoose.model('Order', cartSchema)
